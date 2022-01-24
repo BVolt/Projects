7d5d11cc90603b1252fea8dcc47458738d5a28fa
BVolt <brendenj7@hotmail.com>
2022-01-24 12:36:43 -0800

    second deploy

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/firestore'
 import "firebase/auth"
 
 export const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId:  process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyBvZyp69qen6G1uE4grOgdfgwRGaDfOgj8",
+  authDomain: "watch-list-fb3a1.firebaseapp.com",
+  projectId: "watch-list-fb3a1",
+  storageBucket: "watch-list-fb3a1.appspot.com",
+  messagingSenderId: "981674581597",
+  appId: "1:981674581597:web:9018c469fa7c758776f7b4"
 })
 
 export const authorize = app.auth();
